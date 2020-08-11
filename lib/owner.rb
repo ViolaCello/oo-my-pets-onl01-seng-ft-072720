9e46351b5774e5ff78d60d1785cba4aad3fb6cf3
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 17:25:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,20 @@
 class Owner
   # code goes here
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name = name
     @species = "human"
+    @@all.push(self)
   end
   
   def say_species
     return "I am a human."
+  end
+  
+  def self.all
+    @@all
   end
   
 end
