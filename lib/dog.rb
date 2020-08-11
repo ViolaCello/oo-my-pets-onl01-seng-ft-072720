1067a858a9c721e633b9e23f1a78fe412b626a3f
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 17:53:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog
   # code goes here
-  attr_accessor :owner
+  attr_accessor :owner, :mood 
   attr_reader :name 
   
   def initialize(name, owner)
     @name = name 
     self.owner = owner
+    @mood = "nervous"
+    
   end
 end
